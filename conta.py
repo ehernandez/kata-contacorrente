7a511a70ef0150f3ef65178ab16ad7df6cd22cfb
Émerson Hernandez <emerson@MacBook-Pro.local>
2018-12-18 22:13:13 -0200

    Requisito #1: adicionado teste e função para depositar em conta

--- conta.py
+++ conta.py
@@ -3,3 +3,6 @@ class Conta:
     def __init__(self, id):
         self.id = id
         self.saldo = 0
+
+    def deposita(self, valor):
+        self.saldo = valor;
--- teste_conta.py
+++ teste_conta.py
@@ -1,12 +1,17 @@
 import unittest
-from Conta import Conta
+from python.conta import Conta
 
 
 class TesteConta(unittest.TestCase):
 
     def test_saldo(self):
-        c = Conta(1);
-        self.assertEqual(0, c.saldo);
+        c = Conta(1)
+        self.assertEqual(0, c.saldo)
+
+    def test_deposito(self):
+        c = Conta(2)
+        c.deposita(100)
+        self.assertEqual(100, c.saldo)
 
 
 if __name__ == '__main__':
