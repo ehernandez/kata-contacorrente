275f94363a69c90eca15e4e42b84de510f774235
Émerson Hernandez <emerson@MacBook-Pro.local>
2018-12-18 22:43:29 -0200

    Requisito #5: Refatoração para tirar vírgulas sobrando e retornar status da operação saque para ser usado em transferência

--- conta.py
+++ conta.py
@@ -5,13 +5,14 @@ class Conta:
         self.saldo = saldo
 
     def deposita(self, valor):
-        self.saldo += valor;
+        self.saldo += valor
 
     def saca(self, valor):
         if (valor <= self.saldo):
-            self.saldo -= valor;
+            self.saldo -= valor
+            return True
+        return False
 
     def transfere(self, destino, valor):
-        if (valor <= self.saldo):
-            self.saca(valor)
+        if (self.saca(valor)) :
             destino.deposita(valor)
