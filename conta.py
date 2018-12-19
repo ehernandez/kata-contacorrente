99c666c63477b8c29b425ade533005761e98e0ba
Émerson Hernandez <emerson@MacBook-Pro.local>
2018-12-18 22:48:28 -0200

    Requisito #6: Adicionado atributo limite a conta e parâmetro no construtor. Atualizado teste de saque para levar em consideração o valor do limite.

--- conta.py
+++ conta.py
@@ -1,14 +1,15 @@
 class Conta:
 
-    def __init__(self, id, saldo = 0):
+    def __init__(self, id, saldo = 0, limite = 0):
         self.id = id
         self.saldo = saldo
+        self.limite = limite
 
     def deposita(self, valor):
         self.saldo += valor
 
     def saca(self, valor):
-        if (valor <= self.saldo):
+        if (valor <= (self.saldo + self.limite)):
             self.saldo -= valor
             return True
         return False
